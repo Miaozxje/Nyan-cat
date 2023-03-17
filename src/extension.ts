87fc2969587546453e015f26529abf8e7fba5a66
Cuong Ly <chanhbeck27@gmail.com>
2023-03-17 10:03:48 +0700

    update src

--- src/extension.ts
+++ src/extension.ts
@@ -2,30 +2,29 @@
 
 import * as vscode from 'vscode'
 import * as fs from 'fs'
-import fileUrl from 'file-url'
 import { dirname, normalize } from 'path'
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	const barItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
-	barItem.text = '       '
+	barItem.text = 'Nyan Cat'
 	barItem.tooltip = 'Nyan Cat'
 	barItem.show()
 
-	const htmlDirPath = normalize(`${dirname(require.main.filename)}/vs/code/electron-browser/workbench`)
+	const htmlDirPath = normalize(`${dirname(require.main.filename)}/vs/code/electron-sandbox/workbench`)
 	const htmlFilePath = normalize(`${htmlDirPath}/workbench.html`)
 	const htmlBackupPath = normalize(`${htmlDirPath}/index-nyan-cat-backup.html`)
 
 	// backup index.html
-	backup()
+	// backup()
 
-	// inject js, add unsafe-inline csp
-	injectScript()
+	// // inject js, add unsafe-inline csp
+	// injectScript()
 
-	// inject configuration
-	injectConfiguration()
+	// // inject configuration
+	// injectConfiguration()
 
 	// manual inject configuration command
-	let refreshCMD = vscode.commands.registerCommand('extension.NyanCatRefresh', () => {
+	let refreshCMD = vscode.commands.registerCommand('nyan-cat.NyanCatRefresh', () => {
 		injectConfiguration()
 		vscode.window
 			.showInformationMessage('Nyan Cat: refresh successful, reload Window to take effect.', 'Reload Window')
@@ -35,13 +34,13 @@ export function activate(context: vscode.ExtensionContext) {
 	})
 
 	// uninstall command
-	let uninstallCMD = vscode.commands.registerCommand('extension.NyanCatUninstall', () => {
+	let uninstallCMD = vscode.commands.registerCommand('nyan-cat.NyanCatUninstall', () => {
 		prepareUninstall()
 		vscode.window.showInformationMessage('Ready to uninstall Nyan Cat completed!')
 	})
 
 	// reload command
-	let reloadCMD = vscode.commands.registerCommand('extension.NyanCatReload', () => {
+	let reloadCMD = vscode.commands.registerCommand('nyan-cat.NyanCatReload', () => {
 		try {
 			fs.statSync(htmlBackupPath)
 		} catch (err) {
@@ -77,15 +76,18 @@ export function activate(context: vscode.ExtensionContext) {
 	function injectScript() {
 		let htmlFileContent = fs.readFileSync(htmlFilePath, 'utf-8')
 		if (!htmlFileContent.includes('nyan-cat.js')) {
-			const inject = `<script src="${fileUrl(__dirname + '/nyan-cat.js')}"></script>`
-			htmlFileContent = htmlFileContent.replace('</html>', `${inject}\n</html>`)
-			htmlFileContent = htmlFileContent.replace("script-src 'self'", "script-src 'self' 'unsafe-inline'")
-			fs.writeFileSync(htmlFilePath, htmlFileContent, 'utf-8')
+			import('file-url').then(res => {
+				const inject = `<script src="${res.default(__dirname + '/nyan-cat.js')}"></script>`
+				htmlFileContent = htmlFileContent.replace('</html>', `${inject}\n</html>`)
+				htmlFileContent = htmlFileContent.replace("script-src 'self'", "script-src 'self' 'unsafe-inline'")
+				fs.writeFileSync(htmlFilePath, htmlFileContent, 'utf-8')
+			})
 		}
 	}
 
 	function injectConfiguration() {
 		const config = vscode.workspace.getConfiguration('NyanCat')
+		config.update('backgroundColor', 'black')
 		const inject = `<script id="NyanCatConfiguration">window.NyanCatConfiguration = ${JSON.stringify(
 			config
 		)}</script>`
